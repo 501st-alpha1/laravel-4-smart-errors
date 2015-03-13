3ae72a72a4d2f1f74a6b52190c04cc9b58345ced
Andreas Lutro <anlutro@gmail.com>
2015-03-13 12:02:41 +0100

    initial work on laravel 5 support

--- src/anlutro/L4SmartErrors/Mail/ExceptionMailer.php
+++ src/anlutro/L4SmartErrors/Mail/ExceptionMailer.php
@@ -9,16 +9,19 @@
 
 namespace anlutro\L4SmartErrors\Mail;
 
-use Exception;
-use Illuminate\Foundation\Application;
 use anlutro\L4SmartErrors\AppInfoGenerator;
 use anlutro\L4SmartErrors\Presenters\ExceptionPresenter;
 use anlutro\L4SmartErrors\Presenters\InputPresenter;
 use anlutro\L4SmartErrors\Presenters\QueryLogPresenter;
+use anlutro\L4SmartErrors\Traits\ConfigCompatibilityTrait;
+use Exception;
+use Illuminate\Foundation\Application;
 use Illuminate\Mail\Message;
 
 class ExceptionMailer
 {
+	use ConfigCompatibilityTrait;
+
 	protected $app;
 	protected $exception;
 	protected $appInfo;
@@ -41,11 +44,11 @@ class ExceptionMailer
 
 	public function send($email)
 	{
-		if ($this->app['config']->get('smarterror::force-email')) {
+		if ($this->getConfig('smarterror::force-email')) {
 			$this->app['config']->set('mail.pretend', false);
 		}
 
-		if ($this->app['config']->get('smarterror::expand-stack-trace')) {
+		if ($this->getConfig('smarterror::expand-stack-trace')) {
 			$this->exception->setDescriptive(true);
 		}
 
@@ -60,9 +63,9 @@ class ExceptionMailer
 
 		$exceptionName = $this->getExceptionBaseName($this->exception->getException());
 		$subject = "[$env] $exceptionName - ";
-		$subject .= $this->app['request']->root() ?: $this->app['config']->get('app.url');
-		$htmlView = $this->app['config']->get('smarterror::error-email-view') ?: 'smarterror::error-email';
-		$plainView = $this->app['config']->get('smarterror::error-email-view-plain') ?: 'smarterror::error-email-plain';
+		$subject .= $this->app['request']->root() ?: $this->getConfig('app.url');
+		$htmlView = $this->getConfig('smarterror::error-email-view') ?: 'smarterror::error-email';
+		$plainView = $this->getConfig('smarterror::error-email-view-plain') ?: 'smarterror::error-email-plain';
 
 		$callback = function(Message $msg) use($email, $subject) {
 			$msg->to($email)->subject($subject);
